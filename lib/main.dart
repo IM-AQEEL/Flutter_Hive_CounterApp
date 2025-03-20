e8800eac46f9e1a1a57624d06f5315e56a0a35e7
Muhammad <mohammadaqeel752@gmail.com>
2025-03-20 12:52:20 +0500

    TODO APP

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:hive_practice/screen/home.dart';
 
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
-  await Hive.openBox(' Hive');
+  await Hive.openBox('my box');
   runApp(MyApp());
 }
 
@@ -14,10 +14,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark(),
-      debugShowCheckedModeBanner: false,
-      home: Home(),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Home());
   }
 }
--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -8,54 +8,135 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
-var counter = 0;
-String _counterKey = 'home_key';
-
 class _HomeState extends State<Home> {
+  TextEditingController _controller = new TextEditingController();
+  bool isBoxopen = true;
+  Box? mybox;
+
   @override
   void initState() {
     super.initState();
-    _initializeHive();
+    _openBox();
   }
 
-  Future<void> _initializeHive() async {
-    await Hive.openBox('Hive');
-    final box = Hive.box('Hive');
-    final _value = box.get(_counterKey, defaultValue: 0);
+  // Open Box
+  Future<void> _openBox() async {
+    mybox = await Hive.openBox('my box');
     setState(() {
-      counter = _value;
+      isBoxopen = true;
     });
   }
 
-  @override
+  // Update Operation
+  Future<void> _update(int index, String updateTask) async {
+    if (mybox != null) {
+      await mybox?.putAt(index, updateTask);
+      if (mounted) {
+        {
+          setState(() {});
+        }
+      }
+    }
+  }
+  // Delete Operation
+
+  Future<void> _deleteTask(int index) async {
+    if (mybox != null && index < mybox!.length) {
+      await mybox?.deleteAt(index);
+      setState(() {});
+    }
+  }
+  // Add Task
+
+  Future<void> _addTask(String task) async {
+    mybox?.add(task);
+    setState(() {});
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: increment,
-        label: Text('Increment'),
-      ),
       appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 41, 39, 39),
-        centerTitle: true,
         title: Text(
-          'Hive',
-          style: TextStyle(color: const Color.fromARGB(255, 235, 209, 15)),
+          'ToDo App',
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
         ),
+        toolbarHeight: 150,
+        backgroundColor: Colors.red,
       ),
       body: Center(
-        child: Text(
-          "$counter",
-          style: TextStyle(color: Colors.white, fontSize: 25),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              SizedBox(height: 40),
+              TextField(
+                controller: _controller,
+                decoration: InputDecoration(border: OutlineInputBorder()),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_controller.text.isNotEmpty && isBoxopen) {
+                    _addTask(_controller.text);
+                  }
+                },
+                child: Text('Enter'),
+              ),
+              Expanded(
+                child:
+                    isBoxopen
+                        ? ListView.builder(
+                          itemCount: mybox?.length,
+                          itemBuilder: (context, index) {
+                            return ListTile(
+                              title: Text(mybox?.getAt(index).toString() ?? ''),
+                              trailing: IconButton(
+                                onPressed: () {
+                                  _deleteTask(index);
+                                },
+                                icon: Icon(Icons.delete_outline),
+                              ),
+                              leading: IconButton(
+                                onPressed: () {
+                                  TextEditingController
+                                  controller = new TextEditingController(
+                                    text: mybox?.getAt(index).toString() ?? '',
+                                  );
+                                  showDialog(
+                                    context: context,
+                                    builder:
+                                        (context) => AlertDialog(
+                                          title: Text("Edit Task"),
+                                          content: TextField(
+                                            controller: controller,
+                                          ),
+                                          actions: [
+                                            TextButton(
+                                              onPressed: () {
+                                                _update(index, controller.text);
+                                                Navigator.pop(context);
+                                              },
+                                              child: Text('Edit'),
+                                            ),
+                                          ],
+                                        ),
+                                  );
+                                },
+                                icon: Icon(Icons.edit),
+                              ),
+                            );
+                          },
+                        )
+                        : Container(),
+              ),
+            ],
+          ),
         ),
       ),
     );
-  }
-
-  void increment() {
-    final box = Hive.box('Hive');
-    setState(() {
-      counter = counter + 1;
-    });
-    box.put(_counterKey, counter);
   }
 }
